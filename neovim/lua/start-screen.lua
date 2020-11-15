44d4f797c381de7b969b9405efe1abc61355895f
Brian Ryall <brianryall@fastmail.com>
2020-11-15 13:43:47 -0500

    feat: allow telescope to open buffers from start screnn with line numbers

--- neovim/lua/start-screen.lua
+++ neovim/lua/start-screen.lua
@@ -201,7 +201,7 @@ local function start_screen()
     end
 
     vim.cmd [[noautocmd enew]]
-    vim.cmd [[silent! setlocal bufhidden=wipe colorcolumn= foldcolumn=0 filetype=startify matchpairs= nobuflisted nocursorcolumn nocursorline nolist nonumber norelativenumber nospell noswapfile signcolumn=no synmaxcol& statusline=]]
+    vim.cmd [[silent! setlocal bufhidden=wipe colorcolumn= foldcolumn=0 filetype=startify matchpairs= nocursorcolumn nocursorline nolist nonumber norelativenumber nospell noswapfile signcolumn=no synmaxcol& statusline=]]
     make_sections()
     vim.cmd [[setlocal nomodifiable nomodified]]
     position_cursor()
