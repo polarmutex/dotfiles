a4f351b3fc0d6b20e611492d1df83bd1fe0ad781
Brian Ryall <brianryall@fastmail.com>
2020-11-22 23:42:09 -0500

    feat: packer floating window

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -117,26 +117,7 @@ return require('packer').startup{
 end,
 config = {
     display = {
-        _open_fn = function(name)
-        -- Can only use plenary when we have our plugins.
-        --  We can only get plenary when we don't have our plugins ;)
-        local ok, float_win = pcall(function()
-            return require('plenary.window.float').percentage_range_window(0.8, 0.8)
-        end)
-
-        if not ok then
-            vim.cmd [[65vnew  [packer] ]]
-            return vim.api.nvim_get_current_win(), vim.api.nvim_get_current_buf()
-        end
-
-        local bufnr = float_win.buf
-        local win = float_win.win
-
-        vim.api.nvim_buf_set_name(bufnr, name)
-        vim.api.nvim_win_set_option(win, 'winblend', 10)
-
-        return win, bufnr
-      end
+        open_fn = require('packer.util').float
     },
   }
 }
