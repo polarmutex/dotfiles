8a6c35eb6edb1abf780eef96d5a1fc12908024c9
Brian Ryall <brianryall@fastmail.com>
2021-01-09 11:38:06 -0500

    feat: updated bar for qtile

--- qtile/config.py
+++ qtile/config.py
@@ -24,55 +24,77 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+import os
+import re
+import socket
 from typing import List  # noqa: F401
 
-from libqtile import bar, layout, widget
-from libqtile.config import Click, Drag, Group, Key, Screen
+from libqtile import bar, layout, widget, hook
+from libqtile.config import Click, Drag, Group, Key, KeyChord, Screen
 from libqtile.lazy import lazy
-from libqtile.utils import guess_terminal
 
 mod = "mod4" # sets mod key to super
 myTerminal = "kitty" # my preferred terminal
 
 keys = [
-    Key([mod], "Return", 
-        lazy.spawn(myTerminal), 
+    Key([mod], "Return",
+        lazy.spawn(myTerminal),
         desc="Launch terminal"),
+    Key([mod, "control"], "Return",
+        #lazy.spawn("dmenu_run -p 'Run: '"),
+        lazy.spawn("rofi -show drun -display-drun \"Run: \" -drun-display-format \"{name}\""),
+        desc="Run Launcher"),
+    Key([mod], "w",
+        lazy.window.kill(),
+        desc="Kill focused window"),
+    Key([mod, "control"], "r",
+        lazy.restart(),
+        desc="Restart qtile"),
+    Key([mod, "control"], "q",
+        lazy.shutdown(),
+        desc="Shutdown qtile"),
 
     # Switch between windows in current stack pane
-    Key([mod], "k", lazy.layout.down(),
+    Key([mod], "k",
+        lazy.layout.down(),
         desc="Move focus down in stack pane"),
-    Key([mod], "j", lazy.layout.up(),
+    Key([mod], "j",
+        lazy.layout.up(),
         desc="Move focus up in stack pane"),
 
     # Move windows up or down in current stack
-    Key([mod, "control"], "k", lazy.layout.shuffle_down(),
+    Key([mod, "control"], "k",
+        lazy.layout.shuffle_down(),
         desc="Move window down in current stack "),
-    Key([mod, "control"], "j", lazy.layout.shuffle_up(),
+    Key([mod, "control"], "j",
+        lazy.layout.shuffle_up(),
         desc="Move window up in current stack "),
 
     # Switch window focus to other pane(s) of stack
-    Key([mod], "space", lazy.layout.next(),
+    Key([mod], "space",
+        lazy.layout.next(),
         desc="Switch window focus to other pane(s) of stack"),
 
     # Swap panes of split stack
-    Key([mod, "shift"], "space", lazy.layout.rotate(),
+    Key([mod, "shift"], "space",
+        lazy.layout.rotate(),
         desc="Swap panes of split stack"),
 
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
     # multiple stack panes
-    Key([mod, "shift"], "Return", lazy.layout.toggle_split(),
+    Key([mod, "shift"], "Return",
+        lazy.layout.toggle_split(),
         desc="Toggle between split and unsplit sides of stack"),
 
     # Toggle between different layouts as defined below
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
+    Key([mod], "Tab",
+        lazy.next_layout(),
+        desc="Toggle between layouts"),
 
-    Key([mod, "control"], "r", lazy.restart(), desc="Restart qtile"),
-    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown qtile"),
-    Key([mod], "r", lazy.spawncmd(),
+    Key([mod], "r",
+        lazy.spawncmd(),
         desc="Spawn a command using a prompt widget"),
 ]
 
@@ -94,13 +116,13 @@ for i in groups:
     ])
 
 layouts = [
+    layout.MonadTall(),
     layout.Max(),
     layout.Stack(num_stacks=2),
     # Try more layouts by unleashing below layouts.
     # layout.Bsp(),
     # layout.Columns(),
     # layout.Matrix(),
-    # layout.MonadTall(),
     # layout.MonadWide(),
     # layout.RatioTile(),
     # layout.Tile(),
@@ -109,37 +131,191 @@ layouts = [
     # layout.Zoomy(),
 ]
 
+colors = [["#282c34", "#282c34"], # panel background
+          ["#458588", "#458588"], # background for current screen tab
+          ["#458588", "#458588"], # font color for group names
+          ["#ff5555", "#ff5555"], # border line color for current tab
+          ["#8d62a9", "#8d62a9"], # border line color for other tab and odd widgets
+          ["#668bd7", "#668bd7"], # color for the even widgets
+          ["#458588", "#458588"]] # window name
+
+prompt = "{0}@{1}: ".format(os.environ["USER"], socket.gethostname())
+
+##### DEFAULT WIDGET SETTINGS #####
 widget_defaults = dict(
-    font='sans',
-    fontsize=12,
-    padding=3,
+    font="JetBrains Mono",
+    fontsize = 12,
+    padding = 2,
+    background=colors[2]
 )
 extension_defaults = widget_defaults.copy()
 
-screens = [
-    Screen(
-        bottom=bar.Bar(
-            [
-                widget.CurrentLayout(),
-                widget.GroupBox(),
-                widget.Prompt(),
-                widget.WindowName(),
-                widget.Chord(
-                    chords_colors={
-                        'launch': ("#ff0000", "#ffffff"),
-                    },
-                    name_transform=lambda name: name.upper(),
-                ),
-                widget.TextBox("default config", name="default"),
-                widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
-                widget.Systray(),
-                widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
-                widget.QuickExit(),
-            ],
-            24,
-        ),
-    ),
-]
+
+def init_widgets_list():
+    widgets_list = [
+              widget.Sep(
+                       linewidth = 0,
+                       padding = 6,
+                       foreground = colors[2],
+                       background = colors[0]
+                       ),
+              #widget.Image(
+              #         filename = "~/.config/qtile/icons/python.png",
+              #         mouse_callbacks = {'Button1': lambda qtile: qtile.cmd_spawn('dmenu_run')}
+              #         ),
+              widget.GroupBox(
+                       font = "JetBrains Mono",
+                       fontsize = 9,
+                       margin_y = 3,
+                       margin_x = 0,
+                       padding_y = 5,
+                       padding_x = 3,
+                       borderwidth = 3,
+                       active = colors[2],
+                       inactive = colors[2],
+                       rounded = False,
+                       highlight_color = colors[1],
+                       highlight_method = "text",
+                       this_current_screen_border = colors[3],
+                       this_screen_border = colors [4],
+                       other_current_screen_border = colors[0],
+                       other_screen_border = colors[0],
+                       foreground = colors[2],
+                       background = colors[0]
+                       ),
+              widget.Sep(
+                       linewidth = 1,
+                       padding = 10,
+                       foreground = colors[2],
+                       background = colors[0]
+                       ),
+              widget.WindowName(
+                       foreground = colors[6],
+                       background = colors[0],
+                       padding = 0
+                       ),
+              widget.ThermalSensor(
+                       foreground = colors[2],
+                       background = colors[0],
+                       threshold = 90,
+                       padding = 5
+                       ),
+              widget.Sep(
+                       linewidth = 1,
+                       padding = 10,
+                       foreground = colors[2],
+                       background = colors[0]
+                       ),
+              widget.CheckUpdates(
+                       update_interval = 1800,
+                       foreground = colors[2],
+                       mouse_callbacks = {'Button1': lambda qtile: qtile.cmd_spawn(myTerminal + ' -e sudo pacman -Syu')},
+                       background = colors[0]
+                       ),
+              widget.Sep(
+                       linewidth = 1,
+                       padding = 10,
+                       foreground = colors[2],
+                       background = colors[0]
+                       ),
+              widget.CPU(
+			format = '{load_percent}% ',
+			font = "UbuntuMono Nerd Font",
+                        fontsize = 14,
+			foreground = colors[5],
+                        background = colors[0],
+                        update_interval = 3
+			),
+              widget.Sep(
+                       linewidth = 1,
+                       padding = 10,
+                       foreground = colors[2],
+                       background = colors[0]
+                       ),
+              widget.Memory(
+                       foreground = colors[2],
+                       background = colors[0],
+                       mouse_callbacks = {'Button1': lambda qtile: qtile.cmd_spawn(myTerminal + ' -e htop')},
+                       padding = 5
+                       ),
+              widget.Sep(
+                       linewidth = 1,
+                       padding = 10,
+                       foreground = colors[2],
+                       background = colors[0]
+                       ),
+              widget.TextBox(
+                      text="Vol:",
+                      padding=10,
+                      fontsize=12,
+                       foreground = colors[2],
+                       background = colors[0],
+                      ),
+              widget.Volume(
+                       foreground = colors[2],
+                       background = colors[0],
+                       padding = 5
+                       ),
+              widget.Sep(
+                       linewidth = 1,
+                       padding = 10,
+                       foreground = colors[2],
+                       background = colors[0]
+                       ),
+              widget.CurrentLayoutIcon(
+                       foreground = colors[2],
+                       background = colors[0],
+                       padding = 5
+                       ),
+              widget.Sep(
+                       linewidth = 1,
+                       padding = 10,
+                       foreground = colors[2],
+                       background = colors[0]
+                       ),
+              widget.TextBox(
+                        text = " 📅 ",
+                        padding = 0,
+                        mouse_callbacks = {'Button1': lambda qtile: qtile.cmd_spawn(myTerminal + ' -e calcurse')},
+                        fontsize=12
+                        ),
+
+              widget.Clock(
+                       foreground = colors[2],
+                       background = colors[0],
+                       format = "%A, %B %d  [ %H:%M ]"
+                       ),
+              widget.Sep(
+                       linewidth = 1,
+                       padding = 10,
+                       foreground = colors[2],
+                       background = colors[0]
+                       ),
+              widget.Systray(
+                       background = colors[0],
+                       padding = 5
+                       ),
+              ]
+    return widgets_list
+
+def init_widgets_screen1():
+    widgets_screen1 = init_widgets_list()
+    return widgets_screen1                       # Slicing removes unwanted widgets on Monitors 1,3
+
+def init_widgets_screen2():
+    widgets_screen2 = init_widgets_list()
+    return widgets_screen2                       # Monitor 2 will display all widgets in widgets_list
+
+def init_screens():
+    return [Screen(top=bar.Bar(widgets=init_widgets_screen1(), opacity=1.0, size=20)),
+            Screen(top=bar.Bar(widgets=init_widgets_screen2(), opacity=1.0, size=20)),
+            Screen(top=bar.Bar(widgets=init_widgets_screen1(), opacity=1.0, size=20))]
+
+if __name__ in ["config", "__main__"]:
+    screens = init_screens()
+    widgets_list = init_widgets_list()
+    widgets_screen1 = init_widgets_screen1()
+    widgets_screen2 = init_widgets_screen2()
 
 # Drag floating layouts.
 mouse = [
