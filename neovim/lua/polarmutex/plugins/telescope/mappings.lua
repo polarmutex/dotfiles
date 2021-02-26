f3f916c3da015ef9a62cb1335c1c108c4138acf0
Brian Ryall <brianryall@fastmail.com>
2021-02-26 15:07:05 -0500

    update(neovim) telescope config

--- neovim/lua/polarmutex/plugins/telescope/mappings.lua
+++ neovim/lua/polarmutex/plugins/telescope/mappings.lua
@@ -1,28 +1,62 @@
-local map_tele = function(key, f, options, buffer)
-  local mode = "n"
-  local rhs = string.format(
-    "<cmd>lua require('polarmutex.plugins.telescope')['%s'](%s)<CR>",
-    f,
-    options and vim.inspect(options, { newline = '' }) or ''
-  )
-  local options = {
-    noremap = true,
-    silent = true,
-  }
-
-  if not buffer then
-    vim.api.nvim_set_keymap(mode, key, rhs, options)
-  else
-    vim.api.nvim_buf_set_keymap(0, mode, key, rhs, options)
-  end
+if not pcall(require, 'telescope') then
+  return
 end
 
---vim.api.nvim_set_keymap('c', '<c-r><c-r>', '<Plug>(TelescopeFuzzyCommandSearch)', { noremap = false, nowait = true })
+local sorters = require('telescope.sorters')
+
+TelescopeMapArgs = TelescopeMapArgs or {}
+
+local map_tele = function(key, f, options, buffer)
+    local map_key = vim.api.nvim_replace_termcodes(key .. f, true, true, true)
+
+    TelescopeMapArgs[map_key] = options or {}
+
+    local mode = "n"
+    local rhs = string.format(
+        "<cmd>lua R('polarmutex.plugins.telescope')['%s'](TelescopeMapArgs['%s'])<CR>",
+        f,
+        map_key
+    )
+
+    local map_options = {
+        noremap = true,
+        silent = true,
+    }
+
+    if not buffer then
+        vim.api.nvim_set_keymap(mode, key, rhs, map_options)
+    else
+        vim.api.nvim_buf_set_keymap(0, mode, key, rhs, map_options)
+    end
+end
+
+vim.api.nvim_set_keymap('c', '<c-r><c-r>', '<Plug>(TelescopeFuzzyCommandSearch)', { noremap = false, nowait = true })
+
+-- Search
+map_tele('<space>gw', 'grep_string', {
+  short_path = true,
+  word_match = '-w',
+  only_sort_text = true,
+  layout_strategy = 'vertical',
+  sorter = sorters.get_fzy_sorter(),
+})
+--map_tele('<space>f/', 'grep_last_search', {
+--  layout_strategy = 'vertical',
+--})
 
 -- Files
 map_tele('<leader>sg', 'git_files')
+map_tele('<space>sl', 'live_grep')
+map_tele('<space>sd', 'fd')
+map_tele('<space>pp', 'project_search')
 
 -- Nvim
+map_tele('<space>sb', 'buffers')
+map_tele('<space>sa', 'installed_plugins')
+map_tele('<space>si', 'search_all_files')
+map_tele('<space>ff', 'curbuf')
+map_tele('<space>sh', 'help_tags')
+map_tele('<space>so', 'vim_options')
 
 -- Git / Github
 map_tele('<leader>gi','gh_issues')
