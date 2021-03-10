1194ff183b52585edaf16af0f11589e794878749
Brian Ryall <brianryall@fastmail.com>
2021-03-09 20:18:37 -0500

    fix(neovim) fix lua server

--- neovim/lua/polarmutex/lsp/lua.lua
+++ neovim/lua/polarmutex/lsp/lua.lua
@@ -4,6 +4,15 @@ local custom_attach = require("polarmutex.lsp.attach")
 require('nlua.lsp.nvim').setup(lspconfig, {
     on_attach = custom_attach,
 
+    root_dir = function(fname)
+      if string.find(vim.fn.fnamemodify(fname, ":p"), "neovim/") then
+        return vim.fn.expand("~/repos/dotfiles/neovim/")
+      end
+
+      return lspconfig_util.find_git_ancestor(fname)
+        or lspconfig_util.path.dirname(fname)
+    end,
+
     globals = {
       -- Colorbuddy
       "Color", "c", "Group", "g", "s",
@@ -11,4 +20,4 @@ require('nlua.lsp.nvim').setup(lspconfig, {
       -- Custom
       "RELOAD",
     }
-})
+  })
