e12d349e37c7f464690f1a3d4021f404e79846c2
Brian Ryall <brianryall@fastmail.com>
2021-03-16 16:14:53 -0400

    update(neovim) switch to beancount in the lspconfig

--- neovim/lua/polarmutex/lsp/beancount.lua
+++ neovim/lua/polarmutex/lsp/beancount.lua
@@ -2,5 +2,16 @@ local lspconfig = require('lspconfig')
 local custom_attach = require("polarmutex.lsp.attach")
 
 lspconfig.beancount.setup({
-    on_attach=custom_attach
+    cmd = {
+        'node',
+        '--inspect',
+        '/home/brian/repos/beancount-language-server/out/cli.js',
+        --'beancount-langserver',
+        '--stdio'
+    },
+    on_attach=custom_attach,
+    init_options = {
+      journalFile = "~/repos/beancount/journal.beancount",
+      pythonPath = "~/.cache/pypoetry/virtualenvs/beancount-repo-iwRmyqK8-py3.9/bin/python3";
+    },
 })
