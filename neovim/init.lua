2c1b461963aa2b30e5b8f7c4cc654804b8832425
Brian Ryall <brianryall@fastmail.com>
2021-03-27 00:31:14 -0400

    feat(neovim) restore cursor on bufenter

--- neovim/init.lua
+++ neovim/init.lua
@@ -25,4 +25,13 @@ vim.api.nvim_command('autocmd!')
 vim.api.nvim_command("autocmd VimEnter * ++once lua require('start-screen').start()")
 vim.api.nvim_command('augroup END')
 
-
+-- Put these in an autocmd group, so that you can revert them with:
+-- :augroup vimStartup | au! | augroup END
+vim.api.nvim_command("augroup vimStartup")
+vim.api.nvim_command('autocmd!')
+-- When editing a file, always jump to the last known cursor position.
+-- Don't do it when the position is invalid, when inside an event handler
+-- (happens when dropping a file on gvim) and for a commit message (it's
+-- likely a different one than last time).
+vim.api.nvim_command([[autocmd BufReadPost * if line("'\"") >= 1 && line("'\"") <= line("$") && &ft !~# 'commit' | exe "normal! g`\"" | endif]])
+vim.api.nvim_command("augroup END")
