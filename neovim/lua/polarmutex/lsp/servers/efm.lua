50429780645ffcc2709811b11fd64d2143384f61
Brian Ryall <brianryall@fastmail.com>
2021-03-28 14:19:00 -0400

    update(neovim) efm config

--- neovim/lua/polarmutex/lsp/servers/efm.lua
+++ neovim/lua/polarmutex/lsp/servers/efm.lua
@@ -1,3 +1,24 @@
+-- ESLINT
+local eslint = {
+    lintCommand = "./node_modules/.bin/eslint -f unix --stdin",
+    lintStdin = true
+}
+-- ESLINTD Server
+local eslintd = {
+    lintCommand = "eslint_d -f visualstudio --stdin --stdin-filename ${INPUT}",
+    lintStdin = true,
+    lintFormats = {"%f(%l,%c): %trror %m", "%f(%l,%c): %tarning %m"},
+    lintIgnoreExitCode = true,
+    --formatCommand = "eslint_d --fix-to-stdout --stdin --stdin-filename=${INPUT}",
+    --formatStdin = true
+}
+
+-- PRETTIER
+local prettier = {
+    formatCommand = "./node_modules/.bin/prettier --stdin-filepath=${INPUT}",
+    formatStdin = true
+}
+
 return {
     init_options = {
         documentFormatting = true
@@ -19,33 +40,9 @@ return {
             ".eslintrc.js",
         },
         languages = {
-            javascript = {
-                {
-                    lintCommand = "./node_modules/.bin/eslint -f unix --stdin",
-                    lintStdin = true
-                },
-                {
-                    formatCommand = "./node_modules/.bin/prettier"
-                }
-            },
-            typescript = {
-                {
-                    lintCommand = "./node_modules/.bin/eslint -f unix --stdin",
-                    lintStdin = true
-                },
-                {
-                    formatCommand = "./node_modules/.bin/prettier"
-                }
-            },
-            typescriptreact = {
-                {
-                    lintCommand = "./node_modules/.bin/eslint -f unix --stdin",
-                    lintStdin = true
-                },
-                {
-                    formatCommand = "./node_modules/.bin/prettier"
-                }
-            },
+            javascript = {  eslintd, prettier },
+            typescript = {  eslintd, prettier },
+            typescriptreact = {  eslintd, prettier },
             python = {
                 {
                     formatCommand = "black -",
