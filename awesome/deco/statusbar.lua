40602f7c946eae04e1446dc28fe1382e6f48fd94
Brian Ryall <brianryall@fastmail.com>
2021-03-29 14:28:12 -0400

    fix(awesome) modular layouts

--- awesome/deco/statusbar.lua
+++ awesome/deco/statusbar.lua
@@ -184,10 +184,10 @@ awful.screen.connect_for_each_screen(function(s)
   -- We need one layoutbox per screen.
   s.mylayoutbox = awful.widget.layoutbox(s)
   s.mylayoutbox:buttons(gears.table.join(
-    awful.button({ }, 1, function () awful.layout.inc( 1) end),
-    awful.button({ }, 3, function () awful.layout.inc(-1) end),
-    awful.button({ }, 4, function () awful.layout.inc( 1) end),
-    awful.button({ }, 5, function () awful.layout.inc(-1) end)
+    awful.button({ }, 1, function () awful.layout.inc(RC.layouts, 1) end),
+    awful.button({ }, 3, function () awful.layout.inc(RC.layouts, -1) end),
+    awful.button({ }, 4, function () awful.layout.inc(RC.layouts, 1) end),
+    awful.button({ }, 5, function () awful.layout.inc(RC.layouts, -1) end)
   ))
 
   -- Create a taglist widget
--- awesome/main/layouts.lua
+++ awesome/main/layouts.lua
@@ -1,35 +1,39 @@
 -- Standard awesome library
 local awful = require("awful")
+local lain = require("lain")
 
 local _M = {}
 
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
 function _M.get ()
-  -- Table of layouts to cover with awful.layout.inc, order matters.
-  local layouts = {
+    -- Table of layouts to cover with awful.layout.inc, order matters.
+    local layouts = {
 
-    awful.layout.suit.tile,             -- 2:
---    awful.layout.suit.tile.left,        -- 3:
---    awful.layout.suit.tile.bottom,      -- 4:
---    awful.layout.suit.tile.top,         -- 5:
+        awful.layout.suit.tile,
+        awful.layout.suit.tile.bottom,
+        awful.layout.suit.tile.left,
+        awful.layout.suit.tile.top,
 
-    awful.layout.suit.floating,           -- 1:
+        lain.layout.centerwork,
+        lain.layout.cascade.tile,
 
---    awful.layout.suit.fair,             -- 6:
---    awful.layout.suit.fair.horizontal,  -- 7:
+        --awful.layout.suit.fair,
+        --awful.layout.suit.fair.horizontal,
 
---    awful.layout.suit.spiral,             -- 8:
---    awful.layout.suit.spiral.dwindle,     -- 9:
+        --awful.layout.suit.spiral,
+        --awful.layout.suit.spiral.dwindle,
 
---    awful.layout.suit.max,                -- 10:
---    awful.layout.suit.max.fullscreen,     -- 11:
---    awful.layout.suit.magnifier,          -- 12:
+        --awful.layout.suit.max,
+        --awful.layout.suit.max.fullscreen,
+        --awful.layout.suit.magnifier,
 
---    awful.layout.suit.corner.nw           -- 13:
---    awful.layout.suit.corner.ne,
---    awful.layout.suit.corner.sw,
---    awful.layout.suit.corner.se,
+        --awful.layout.suit.corner.nw
+        --awful.layout.suit.corner.ne,
+        --awful.layout.suit.corner.sw,
+        --awful.layout.suit.corner.se,
+
+        awful.layout.suit.floating,
   }
 
   return layouts
