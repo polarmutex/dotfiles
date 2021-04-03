d572ccc65b5f2bd3e37e5d8f185895077a44d3a3
Brian Ryall <brianryall@fastmail.com>
2021-04-03 14:23:46 -0400

    fix(awesome) wallpaper on load

--- awesome/modules/set-wallpaper.lua
+++ awesome/modules/set-wallpaper.lua
@@ -3,15 +3,16 @@ local awful = require("awful")
 local beautiful = require("beautiful")
 
 local function set_wallpaper(s)
-  -- Wallpaper
-  if beautiful.wallpaper then
-    local wallpaper = beautiful.wallpaper
-    -- If wallpaper is a function, call it with the screen
-    if type(wallpaper) == "function" then
-      wallpaper = wallpaper(s)
-    end
-    gears.wallpaper.maximized(wallpaper, s, true)
-  end
+    awful.spawn.with_shell("feh --bg-fill --randomize ~/Media/wallpapers/*")
+    -- Wallpaper
+     --if beautiful.wallpaper then
+        --local wallpaper = beautiful.wallpaper
+        -- If wallpaper is a function, call it with the screen
+        --if type(wallpaper) == "function" then
+        --    wallpaper = wallpaper(s)
+        --end
+        --gears.wallpaper.maximized(wallpaper, s, true)
+    --end
 end
 
 -- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
