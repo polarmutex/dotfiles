729256b687da56e8f460ce1c19a9d2dfef87b9f6
Brian Ryall <brianryall@fastmail.com>
2021-04-02 22:27:41 -0400

    feat(neovim) add global awesome to sumneko settings

--- neovim/lua/polarmutex/lsp/servers/sumneko.lua
+++ neovim/lua/polarmutex/lsp/servers/sumneko.lua
@@ -8,13 +8,22 @@ return {
             },
             diagnostics = {
                 -- Get the language server to recognize the `vim` global
-                globals = {'vim'},
+                globals = {
+                    'vim',
+                    --Packer
+                    'use',
+                    --Awesome
+                    'awesome',
+                    'client',
+                    'root'
+                },
             },
             workspace = {
                 -- Make the server aware of Neovim runtime files
                 library = {
                     [vim.fn.expand('$VIMRUNTIME/lua')] = true,
                     [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+                    [vim.fn.expand('/usr/share/awesome/lib')] = true,
                 },
             },
         }
