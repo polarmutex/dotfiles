caa219e6717de168742ebad586bd49c7e4b8c045
Brian Ryall <brianryall@fastmail.com>
2021-04-29 16:59:25 -0400

    update(neovim) move spell check to lua

--- dot_config/nvim/executable_init.lua
+++ dot_config/nvim/executable_init.lua
@@ -20,6 +20,7 @@ require("polarmutex.mappings")
 --Modules
 require("polarmutex.modules.whitespace").setup()
 require("polarmutex.modules.terminal")
+require("polarmutex.modules.spell")
 
 vim.api.nvim_command("augroup start_screen")
 vim.api.nvim_command("autocmd!")
