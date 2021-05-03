09136dc2e8e34ca05c6769107964e54c1eeb6726
Brian Ryall <brianryall@fastmail.com>
2021-05-03 16:19:19 -0400

    update(zsh) add color to ls commands

--- dot_config/zsh/plugins/common-aliases.plugin.zsh
+++ dot_config/zsh/plugins/common-aliases.plugin.zsh
@@ -1,14 +1,14 @@
 
 # ls, the common ones I use a lot shortened for rapid fire usage
-alias l='ls -lFh'     #size,show type,human readable
-alias la='ls -lAFh'   #long list,show almost all,show type,human readable
-alias lr='ls -tRFh'   #sorted by date,recursive,show type,human readable
-alias lt='ls -ltFh'   #long list,sorted by date,show type,human readable
-alias ll='ls -l'      #long list
-alias ldot='ls -ld .*'
-alias lS='ls -1FSsh'
-alias lart='ls -1Fcart'
-alias lrt='ls -1Fcrt'
+alias l='ls -lFh --color'     #size,show type,human readable
+alias la='ls -lAFh --color'   #long list,show almost all,show type,human readable
+alias lr='ls -tRFh --color'   #sorted by date,recursive,show type,human readable
+alias lt='ls -ltFh --color'   #long list,sorted by date,show type,human readable
+alias ll='ls -l --color'      #long list
+alias ldot='ls -ld .* --color'
+alias lS='ls -1FSsh --color'
+alias lart='ls -1Fcart --color'
+alias lrt='ls -1Fcrt --color'
 
 alias zshrc='${=EDITOR} ${ZDOTDIR:-$HOME}/.zshrc' # Quick access to the .zshrc file
 
