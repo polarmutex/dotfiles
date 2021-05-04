325433968e3200f6a22e3309854b632256bf8472
Brian Ryall <brian.ryall@udri.udayton.edu>
2021-05-04 11:33:04 -0400

    update(neovim) move autocmd to modules

--- dot_config/nvim/executable_init.lua
+++ dot_config/nvim/executable_init.lua
@@ -21,6 +21,9 @@ local modules = {
     "polarmutex.modules.spell",
     "polarmutex.modules.terminal",
     "polarmutex.modules.statusline",
+    "polarmutex.modules.cur-col-line",
+    "polarmutex.modules.cursor-last-edit",
+    "polarmutex.modules.highlight-yank",
 }
 
 local errors = {}
@@ -37,31 +40,13 @@ if not vim.tbl_isempty(errors) then
     end
 end
 
-vim.api.nvim_command("augroup start_screen")
-vim.api.nvim_command("autocmd!")
-vim.api.nvim_command("autocmd VimEnter * ++once lua require('polarmutex.modules.start-screen').start()")
-vim.api.nvim_command("augroup END")
-
--- Put these in an autocmd group, so that you can revert them with:
--- :augroup vimStartup | au! | augroup END
-vim.api.nvim_command("augroup vimStartup")
-vim.api.nvim_command("autocmd!")
--- When editing a file, always jump to the last known cursor position.
--- Don't do it when the position is invalid, when inside an event handler
--- (happens when dropping a file on gvim) and for a commit message (it's
--- likely a different one than last time).
-vim.api.nvim_command([[
-autocmd BufReadPost * if line("'\"") >= 1 && line("'\"") <= line("$") && &ft !~# 'commit' | exe "normal! g`\"" | endif
-]])
-vim.api.nvim_command("augroup END")
+vim.api.nvim_exec([[
+augroup start_screen
+    autocmd!
+    autocmd VimEnter * ++once lua require('polarmutex.modules.start-screen').start()
+augroup END
+]], false)
 
 -- automatically run packer compile on plugin change
-vim.api.nvim_command("autocmd BufWritePost plugins.lua PackerCompile")
+vim.api.nvim_command("autocmd BufWritePost packer.lua PackerCompile")
 
--- highlight yank
-vim.api.nvim_command("augroup LuaHighlight")
-vim.api.nvim_command("autocmd!")
-vim.api.nvim_command([[
-autocmd TextYankPost * lua vim.highlight.on_yank { hlgroup = "Substitute", timeout = 150, on_macro = true }
-]])
-vim.api.nvim_command("augroup END")
